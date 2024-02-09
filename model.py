a94de56e260798cd204c40651d3fa4c7af13bf45
Mads Frandsen <madschristianfrandsen@gmail.com>
2024-02-09 17:00:38 +0100

    context unet

--- model.py
+++ model.py
@@ -101,3 +101,66 @@ class UnetDown(nn.Module):
     
     def forward(self, x):
         return self.model(x)
+    
+
+class EmbedFC(nn.Moduel):
+    def __init__(self, input_dim, emb_dim):
+        super(EmbedFC, self).__init__()
+
+        self.input_dim = input_dim
+        layers = [
+            nn.Linear(input_dim, emb_dim),
+            nn.GELU(),
+            nn.Linear(emb_dim, emb_dim)
+        ]
+
+        self.model = nn.Sequential(*layers)
+    
+    def forward(self, x):
+        x = x.view(-1, self.input_dim)
+        return self.model(x)
+    
+
+
+class ContextUnet(nn.Moduel):
+    def __init__(self, in_channels: int, n_feat=256, n_cfeat=10, img_size=28, nb_class=3):
+        super(ContextUnet, self).__init__()
+
+        self.in_channels = in_channels
+        self.n_feat = n_feat
+        self.n_cfeat = n_cfeat
+        self.h = img_size #assume h == w. must be divisible by 4, so 28,24,20,16...
+        self.nb_class = nb_class
+
+        self.embedding = nn.Embedding(self.nb_class, in_channels)
+        self.init_conv = ResidualConvBlock(in_channels, n_feat, is_res=True)
+
+        self.down1 = UnetDown(n_feat, n_feat)
+        self.down2 = UnetDown(n_feat, 2 * n_feat)
+
+        self.to_vec = nn.Sequential(nn.AvgPool2d((4)), nn.GELU())
+
+        self.timeembed1 = EmbedFC(1, 2 * n_feat)
+        self.timeembed2 = EmbedFC(1, n_feat)
+        self.contextembed1 = EmbedFC(n_cfeat, 2 * n_feat)
+        self.contextembed2 = EmbedFC(n_cfeat, n_feat)
+
+        self.up0 = nn.Sequential(
+            nn.ConvTranspose2d(2 * n_feat, 2 * n_feat, self.h//4),
+            nn.GroupNorm(8, 2 * n_feat),
+            nn.ReLU()
+        )
+
+        self.up1 = UnetUp(4 * n_feat, n_feat)
+        self.up2 = UnetUp(2 * n_feat, n_feat)
+
+        self.out = nn.Sequential(
+            nn.Conv2d(2 * n_feat, n_feat, 3, 1, 1),
+            nn.GroupNorm(8, n_feat),
+            nn.ReLU(),
+            nn.Conv2d(n_feat, nb_class, 3, 1, 1)
+        )
+
+
+    def forward(self, x, t, c=None):
+        pass
