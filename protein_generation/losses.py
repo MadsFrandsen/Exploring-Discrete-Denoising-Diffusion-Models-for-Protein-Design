6b1a785b0203ce19db5228803d64f8547bbf2aa8
Mads Frandsen <madschristianfrandsen@gmail.com>
2024-03-18 17:13:37 +0100

    loss function

---
+++ protein_generation/losses.py
@@ -0,0 +1,75 @@
+import torch
+from torch.nn import CrossEntropyLoss, KLDivLoss
+from utils import Tokenizer
+from constants import PROTEIN_ALPHABET
+
+def sample_prior(a, b, len=len(PROTEIN_ALPHABET)):
+    """
+    Returns prior for KL at T -> inf with same shape as q over total possible values
+    Prior is a uniform distribution over number of values
+    """
+    prior = torch.empty(a, b)
+    prior = torch.ones_like(prior) / len
+    return prior
+
+
+class D3PMLVBLoss(KLDivLoss):
+    """
+    Shape:
+        Inputs:
+            - src_one_hot: (B, L, K) original seq one hot encoded
+            - q: (B, L, K) forward prob dist
+            - predictions: (B, L, K) model predictions
+            - tgt: (B, L) corrupted MSA tokenized
+            - tgt_one_hot: (B, L, K) corrupted MSA one hot encoded
+            - timestep (B)
+            - Q (K, K) transition matrix
+            - Q_bar (K, K) transition matrix accounting for time
+    """
+    def __init__(self, tmax=500, reduction='batchmean', log_target=False, tokenizer=Tokenizer()):
+        self.tmax = tmax
+        self.tokenizer = Tokenizer
+        self.K = self.tokenizer.K
+        super().__init__(reduction=reduction, log_target=log_target)
+    
+    def forward(self, src_onehot, q, predicitons, tgt, tgt_onehot, timestep, Q, Q_bar):
+        p = torch.nn.functional.softmax(predicitons[:, :, :self.K], dim=2)
+        losses = []
+        for i in range(tgt.shape[0]): # loop over batch
+            if timestep[i] == 1:
+                # CE (L_t=0)
+                r_loss = CrossEntropyLoss(predicitons[i].unsqueeze(0), tgt[i].unsqueeze(0))
+                losses.append(r_loss)
+            elif timestep[i] == self.tmax:
+                # D KL (L_T)
+                prior = sample_prior(q[i].shape[0], q[i].shape[1], len=self.K)
+                prior = prior.to(tgt.device)
+                kl_loss_i = super().forward(prior.log(), q[i])
+                losses.append(kl_loss_i)
+            else:
+                # D KL (L_t-1) -> (q(x|x_t, x_0), p_theta)
+                pred = p[i]
+                x_t = src_onehot[i]
+                x_0 = tgt_onehot[i]
+
+                A = torch.mm(x_t, torch.t(Q[timestep[i]]))
+                B = torch.mm(x_0, Q_bar[timestep[i] - 1])
+                Q_expand = Q_bar[timestep[i] - 1].unsqueeze(0).expand(A.shape[0], self.K, self.K)
+                B_pred = torch.mul(pred.unsqueeze(2), Q_expand)
+
+                q_t = torch.mul(A.unsqueeze(1), B_pred)
+                p_theta_marg = torch.bmm(torch.transpose(q_t, 1,2), pred.unsqueeze(2)).squeeze()
+
+                num = torch.mul(A, B)
+                denom = torch.bmm(torch.mm(x_0, Q_bar[timestep[i]]).unsqueeze(1), x_t.unsqueeze(2))
+                q_t_minus1 = num / denom.squeeze().unsqueeze(1)
+
+                p_theta_marg = p_theta_marg / p_theta_marg.sum(dim=1, keepdim=True)
+                p_theta_marg = p_theta_marg.to(tgt.device)
+                kl_loss_i = super().forward(p_theta_marg.log(), q_t_minus1)
+                losses.append(kl_loss_i)
+        losses = torch.stack(losses)
+        lvb = ((losses.sum() / tgt.shape[0]))
+        return lvb
+
+
--- protein_generation/parser.py
+++ protein_generation/parser.py
@@ -7,12 +7,15 @@ import torch
 from torch.utils.data import Dataset
 from sklearn.model_selection import train_test_split
 from constants import ALL_AAS, AMB_AAS, CAN_AAS
+from utils import Tokenizer
 
+# path to dataset
 path = '/Users/madsfrandsen/Documents/BSc_project/protein_generation/alignments/BLAT_ECOLX_1_b0.5.a2m'
 
 
 def parse_fasta(path):
     sequences = []
+    names = []
     with open(path, 'r') as file:
         sequence = ''
         for line in file:
@@ -21,6 +24,7 @@ def parse_fasta(path):
             # If line is a new sequence we append the previous sequence,
             # and reset the sequence to the empty string.
             if line.startswith('>'):
+                names.append(line)
                 if sequence:
                     sequences.append(sequence)
                     sequence = ''
@@ -32,36 +36,42 @@ def parse_fasta(path):
             sequences.append(sequence)
     
     # Convert to Numpy array and return
-    return np.array(sequences)
+    return np.array(sequences), np.array(names)
 
 
-data = parse_fasta(path=path)
+data, names = parse_fasta(path=path)
 
-train_data, test_data = train_test_split(data, test_size=0.20, random_state=42)
+train_data, train_names, test_data, test_names = train_test_split(
+    data, names, test_size=0.20, random_state=42)
 
 
 class BlAT_ECOLX_1(Dataset):
     def __init__(self, train=True):
-        self.data = parse_fasta(path=path)
+        self.data = train_data if train else test_data
+        self.names = train_names if train else test_names
 
     def __len__(self):
         return len(self.data)
     
     def __getitem__(self, idx):
-        return self.data[idx]
+        data = self.data[idx]
+        name = self.names[idx]
+        return data, name
 
 
-for seq in data:
-    if any(aa in AMB_AAS for aa in seq):
-        print('Found AA')
 
 
-for seq in data:
-    for aa in seq:
-        if aa == 'Z':
-            print(seq)
+# for seq in data:
+#     if any(aa in AMB_AAS for aa in seq):
+#         print('Found AA')
 
 
-for seq in data:
-    print(seq)
+# for seq in data:
+#     for aa in seq:
+#         if aa == 'Z':
+#             print(seq)
+
+
+# for seq in data:
+#     print(seq)
 
